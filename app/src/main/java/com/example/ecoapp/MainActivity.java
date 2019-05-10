89015acd76256bcfd7360cc78309e51c86e96629
Tomikoma <szabotomika97@gmail.com>
2019-05-10 16:06:19 +0200

    SMSReceiver added/implemented

--- app/src/main/java/com/example/ecoapp/MainActivity.java
+++ app/src/main/java/com/example/ecoapp/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.ecoapp;
 
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BroadcastReceiver br;
+
+    private static final int MY_PERMISSIONS_REQUEST_RECEIVE_SMS = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.RECEIVE_SMS)){
+
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECEIVE_SMS}, MY_PERMISSIONS_REQUEST_RECEIVE_SMS);
+            }
+        }
+
+        this.br = new SmsReceiver();
+        IntentFilter filter = new IntentFilter("android.provider.Telephony.SMS_RECEIVED");
+        this.registerReceiver(this.br,filter);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+
+        switch (requestCode){
+            case MY_PERMISSIONS_REQUEST_RECEIVE_SMS:
+            {
+                if(grantResults.length> 0 && grantResults[0] ==  PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Thank you for permitting!", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(this, "Igy nem fog mukodni az applikacio.", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
     }
 }
